7c615d562c0eb7036b226320aa5ffcc48594c1cb
AlperO06 <alperozdemir689@gmail.com>
2025-11-29 17:48:50 +0000

    Add AboutPage widget with basic layout and structure

---
+++ lib/about_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+/// A simple About page scaffold.
+///
+/// Created so it can be imported and used from other parts of the app.
+class AboutPage extends StatelessWidget {
+  const AboutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+      ),
+      body: const Center(
+        child: Text('About page'),
+      ),
+    );
+  }
+}
