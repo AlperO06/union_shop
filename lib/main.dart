dfab07c56ba7b9b88421799e39a40ca001d329f7
AlperO06 <alperozdemir689@gmail.com>
2025-12-01 22:43:37 +0000

    Update HomeScreen button to navigate to Sale page

--- lib/main.dart
+++ lib/main.dart
@@ -175,7 +175,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                   const SizedBox(width: 12),
                   ElevatedButton(
-                    onPressed: placeholderCallbackForButtons,
+                    onPressed: () => Navigator.pushNamed(context, '/sale'),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color(0xFF4d2963),
                       foregroundColor: Colors.white,
