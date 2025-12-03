50e3d303c9958ee9e9dbe830c8e565031ef4e0d1
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 01:21:48 +0000

    Added login import to main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'about_page.dart';
 import 'collections_page.dart';
 import 'sale_page.dart';
 import 'screens/signup_page.dart';
+import 'screens/login_page.dart';
 
 void main() {
   runApp(const UnionShopApp());
