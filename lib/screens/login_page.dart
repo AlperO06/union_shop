81df56a8a0390ec08d5bcc627568a3d6aa0972c1
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 00:54:56 +0000

    Add initial implementation of the login page

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailCtrl = TextEditingController();
+  final _passwordCtrl = TextEditingController();
+  bool _obscure = true;
+  bool _loading = false;
+
+  @override
+  void dispose() {
+    _emailCtrl.dispose();
+    _passwordCtrl.dispose();
+    super.dispose();
+  }
+
+ 
