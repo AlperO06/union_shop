89e5f208991059ab7deb9a0d6273daabd4a6ab81
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 01:07:40 +0000

    Initialize SignupPage with form fields and state management

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameCtrl = TextEditingController();
+  final _emailCtrl = TextEditingController();
+  final _passwordCtrl = TextEditingController();
+  final _confirmCtrl = TextEditingController();
+
+  bool _obscurePassword = true;
+  bool _loading = false;
+
