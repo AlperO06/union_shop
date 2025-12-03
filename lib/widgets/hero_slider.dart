9a65e5c75a250c22212dbb09a47a5a845d924934
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 23:49:37 +0000

    Started HeroSlider class

--- lib/widgets/hero_slider.dart
+++ lib/widgets/hero_slider.dart
@@ -9,3 +9,20 @@ class HeroSlide {
   final VoidCallback? onSecondary;
   final String primaryLabel;
   final String? secondaryLabel;
+
+  const HeroSlide({
+    required this.imageUrl,
+    required this.title,
+    this.subtitle,
+    this.onPrimary,
+    this.onSecondary,
+    this.primaryLabel = 'Shop now',
+    this.secondaryLabel,
+  });
+}
+
+class HeroSlider extends StatefulWidget {
+  final List<HeroSlide> slides;
+  final double height;
+  final Duration autoPlayInterval;
+  final bool autoPlay;
