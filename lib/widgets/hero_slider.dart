751b435d19233d70524221b2c2efe5c44ad824b7
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 23:49:08 +0000

    Add HeroSlide class with properties for image and labels

--- lib/widgets/hero_slider.dart
+++ lib/widgets/hero_slider.dart
@@ -1,2 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+
+class HeroSlide {
+  final String imageUrl;
+  final String title;
+  final String? subtitle;
+  final VoidCallback? onPrimary;
+  final VoidCallback? onSecondary;
+  final String primaryLabel;
+  final String? secondaryLabel;
