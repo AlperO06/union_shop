f792d4a28de2b4524f105b8295fdfe480c002c12
AlperO06 <alperozdemir689@gmail.com>
2025-12-03 17:45:29 +0000

    Add CartPage class with initial items list

--- lib/screens/Cart.dart
+++ lib/screens/Cart.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/material.dart';
 import '../widgets/union_page_scaffold.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final items = [
+      {
+        'name': 'Limited Edition Essential Zip Hoodie',
+        'size': 'M',
+        'quantity': 1,
+        'price': 59.99,
+      },
+      {
+        'name': 'Essential T-Shirt',
+        'size': 'L',
+        'quantity': 2,
+        'price': 24.99,
+      },
+      {
+        'name': 'Portsmouth City Postcard',
+        'size': 'One Size',
+        'quantity': 3,
+        'price': 1.00,
+      },
+    ];
