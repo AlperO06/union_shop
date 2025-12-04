73574da66209f444dcc0c1ad33677cfac7304fa1
AlperO06 <alperozdemir689@gmail.com>
2025-12-04 23:25:36 +0000

    Remove Product class definition from data file and retain product list

--- lib/data/products.dart
+++ lib/data/products.dart
@@ -1,29 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-class Product {
-  final int id;
-  final String name;
-  final String image;
-  final double oldPrice;
-  final double newPrice;
-  final String description;
-  final List<String>? images;
-  final List<String>? sizes;
-  final List<String>? colors;
-
-  const Product({
-    required this.id,
-    required this.name,
-    required this.image,
-    required this.oldPrice,
-    required this.newPrice,
-    required this.description,
-    this.images,
-    this.sizes,
-    this.colors,
-  });
-}
-
 const List<Product> products = [
   Product(
     id: 1,
