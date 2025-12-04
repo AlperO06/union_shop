4f688e255679ec08118f2503a8fc032ddc90bfb9
AlperO06 <alperozdemir689@gmail.com>
2025-12-04 03:17:51 +0000

    Created class print shack

--- lib/screens/print_shack_product_page.dart
+++ lib/screens/print_shack_product_page.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 import '../data/print_shack.dart';
+
+class PrintShackProductPage extends StatefulWidget {
+  final PrintShackProduct product;
+
+  const PrintShackProductPage({Key? key, required this.product}) : super(key: key);
+
+  @override
+  State<PrintShackProductPage> createState() => _PrintShackProductPageState();
+}
+
+class _PrintShackProductPageState extends State<PrintShackProductPage> {
+  int _selectedImage = 0;
+  int _lines = 1;
+  int _quantity = 1;
+  late List<TextEditingController> _controllers;
