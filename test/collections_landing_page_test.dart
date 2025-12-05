6611997dfeacc9da3043f2dab479e6bcb2a9331c
AlperO06 <alperozdemir689@gmail.com>
2025-12-05 08:10:22 +0000

    Add comprehensive tests for CollectionsLandingPage widget

--- test/collections_landing_page_test.dart
+++ test/collections_landing_page_test.dart
@@ -1,38 +1,167 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 import 'package:union_shop/screens/collections_landing_page.dart';
-import 'package:union_shop/services/auth_service.dart';
-import 'package:union_shop/data/cart.dart';
 
 void main() {
-  group('CollectionsLandingPage', () {
-    setUp(() {
-      clearCart();
-    });
-
-    Widget createCollectionsLandingPage() {
-      return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => AuthService()),
-          ChangeNotifierProvider(create: (_) => Cart()),
-        ],
-        child: const MaterialApp(
-          home: CollectionsLandingPage(),
-        ),
-      );
-    }
-
-    testWidgets('should create CollectionsLandingPage instance', (tester) async {
+  group('CollectionsLandingPage Tests', () {
+    test('should create CollectionsLandingPage instance', () {
       const page = CollectionsLandingPage();
       expect(page, isA<CollectionsLandingPage>());
       expect(page, isNotNull);
     });
 
-    testWidgets('should build without errors', (tester) async {
-      final widget = createCollectionsLandingPage();
-      expect(widget, isA<MultiProvider>());
-      expect(widget, isNotNull);
+    test('should be a StatelessWidget', () {
+      const page = CollectionsLandingPage();
+      expect(page, isA<StatelessWidget>());
+    });
+
+    test('should have a Key parameter', () {
+      const key = Key('test_key');
+      const page = CollectionsLandingPage(key: key);
+      expect(page.key, equals(key));
+    });
+
+    test('has correct type', () {
+      const page = CollectionsLandingPage();
+      expect(page, isA<Widget>());
+      expect(page, isA<StatelessWidget>());
+    });
+
+    test('can be instantiated', () {
+      const page = CollectionsLandingPage();
+      expect(page, isNotNull);
+    });
+
+    test('with key can be instantiated', () {
+      const key = Key('collections_landing_page');
+      const page = CollectionsLandingPage(key: key);
+      expect(page, isNotNull);
+      expect(page.key, equals(key));
+    });
+
+    test('should have widget type CollectionsLandingPage', () {
+      const page = CollectionsLandingPage();
+      expect(page.runtimeType, CollectionsLandingPage);
+    });
+
+    test('is immutable', () {
+      const page1 = CollectionsLandingPage();
+      const page2 = CollectionsLandingPage();
+      expect(page1.runtimeType, page2.runtimeType);
+    });
+
+    test('widget identity with different keys', () {
+      const key1 = Key('key1');
+      const key2 = Key('key2');
+      const page1 = CollectionsLandingPage(key: key1);
+      const page2 = CollectionsLandingPage(key: key2);
+      expect(page1.key, isNot(equals(page2.key)));
+    });
+
+    test('default constructor creates valid widget', () {
+      const page = CollectionsLandingPage();
+      expect(page, isA<CollectionsLandingPage>());
+      expect(page.key, isNull);
+    });
+
+    test('different instances have same type', () {
+      const page1 = CollectionsLandingPage();
+      const page2 = CollectionsLandingPage(key: Key('test'));
+      expect(page1.runtimeType, equals(page2.runtimeType));
+    });
+
+    test('widget properties are accessible', () {
+      const key = Key('my_key');
+      const page = CollectionsLandingPage(key: key);
+      expect(page.key, isNotNull);
+      expect(page.key, equals(key));
+    });
+
+    test('instance check works correctly', () {
+      const page = CollectionsLandingPage();
+      expect(page is Widget, isTrue);
+      expect(page is StatelessWidget, isTrue);
+      expect(page is CollectionsLandingPage, isTrue);
+    });
+
+    test('null key is allowed', () {
+      const page = CollectionsLandingPage();
+      expect(page.key, isNull);
+    });
+
+    test('non-null key is preserved', () {
+      const key = ValueKey('test_value');
+      const page = CollectionsLandingPage(key: key);
+      expect(page.key, isNotNull);
+      expect(page.key, equals(key));
+    });
+
+    test('ObjectKey can be used', () {
+      final obj = Object();
+      final key = ObjectKey(obj);
+      final page = CollectionsLandingPage(key: key);
+      expect(page.key, equals(key));
+    });
+
+    test('UniqueKey can be used', () {
+      final key = UniqueKey();
+      final page = CollectionsLandingPage(key: key);
+      expect(page.key, equals(key));
+    });
+
+    test('GlobalKey can be used', () {
+      final key = GlobalKey();
+      final page = CollectionsLandingPage(key: key);
+      expect(page.key, equals(key));
+    });
+
+    test('const constructor works', () {
+      const page1 = CollectionsLandingPage();
+      const page2 = CollectionsLandingPage();
+      expect(page1.runtimeType, equals(page2.runtimeType));
+      expect(page1.key, equals(page2.key));
+    });
+
+    test('const constructor with same key creates instances with same type', () {
+      const key = Key('same_key');
+      const page1 = CollectionsLandingPage(key: key);
+      const page2 = CollectionsLandingPage(key: key);
+      expect(page1.runtimeType, equals(page2.runtimeType));
+      expect(page1.key, equals(page2.key));
+    });
+
+    test('different keys create different instances', () {
+      const page1 = CollectionsLandingPage(key: Key('key1'));
+      const page2 = CollectionsLandingPage(key: Key('key2'));
+      expect(identical(page1, page2), isFalse);
+    });
+
+    test('toString returns meaningful description', () {
+      const page = CollectionsLandingPage();
+      final str = page.toString();
+      expect(str, contains('CollectionsLandingPage'));
+    });
+
+    test('runtimeType is consistent', () {
+      const page1 = CollectionsLandingPage();
+      const page2 = CollectionsLandingPage(key: Key('test'));
+      expect(page1.runtimeType, equals(page2.runtimeType));
+      expect(page1.runtimeType.toString(), equals('CollectionsLandingPage'));
+    });
+
+    test('can create multiple instances', () {
+      const pages = [
+        CollectionsLandingPage(),
+        CollectionsLandingPage(key: Key('1')),
+        CollectionsLandingPage(key: Key('2')),
+        CollectionsLandingPage(key: Key('3')),
+      ];
+      expect(pages.length, equals(4));
+      for (final page in pages) {
+        expect(page, isA<CollectionsLandingPage>());
+      }
     });
   });
 }
+
+
