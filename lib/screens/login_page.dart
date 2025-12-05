a2fb83efbb4a8ce65701dccb9eb4ee0d79a04c14
AlperO06 <alperozdemir689@gmail.com>
2025-12-05 00:20:00 +0000

    Add provider and auth_service imports to LoginPage for state management

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../services/auth_service.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -23,6 +25,8 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign In'),
