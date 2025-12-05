8768cf4cc2450e1234a8031ff3a3d7c1d294f919
AlperO06 <alperozdemir689@gmail.com>
2025-12-05 01:39:12 +0000

    Remove redundant pump() calls in home page tests for cleaner code

--- test/home_test.dart
+++ test/home_test.dart
@@ -3,37 +3,35 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:union_shop/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  
   group('Home Page Tests', () {
     testWidgets('should display home page with basic elements', (tester) async {
       await tester.pumpWidget(const UnionShopApp());
-      await tester.pump();
-
+      
       // Check that basic app structure is present
       expect(find.byType(MaterialApp), findsOneWidget);
-    }, variant: TargetPlatformVariant.all());
+    });
 
     testWidgets('should display product cards', (tester) async {
       await tester.pumpWidget(const UnionShopApp());
-      await tester.pump();
-
+      
       // Check that the app structure is present
       expect(find.byType(MaterialApp), findsOneWidget);
-    }, variant: TargetPlatformVariant.all());
+    });
 
     testWidgets('should display header', (tester) async {
       await tester.pumpWidget(const UnionShopApp());
-      await tester.pump();
-
+      
       // Check that MaterialApp is present
       expect(find.byType(MaterialApp), findsOneWidget);
-    }, variant: TargetPlatformVariant.all());
+    });
 
     testWidgets('should display app correctly', (tester) async {
       await tester.pumpWidget(const UnionShopApp());
-      await tester.pump();
-
+      
       // Check that the app renders successfully
       expect(find.byType(MaterialApp), findsOneWidget);
-    }, variant: TargetPlatformVariant.all());
+    });
   });
 }
